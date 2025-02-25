2e97b5b454fb629d2949657a399d22fb72b27f7e
Kamaal ABOOTHALIB <kamaal@kamaal.me>
2025-02-25 13:19:58 +0530

    hotfix: image fix

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
 import RHFLogin from "@/components/rhf/rhf-login";
+import Image from "next/image";
 
 export function LoginForm({
   className,
@@ -13,11 +14,10 @@ export function LoginForm({
         <CardContent className="grid p-0 md:grid-cols-2">
           <RHFLogin />
           <div className="relative hidden bg-muted md:block">
-            <img
-              alt={":"}
+            <Image
               src="https://images.unsplash.com/photo-1604028297236-42130c7dcc3a?q=80&w=4000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Image"
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              fill={true}
             />
           </div>
         </CardContent>
